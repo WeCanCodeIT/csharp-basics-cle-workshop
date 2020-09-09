9d04d25fd960d904bfc0efffa865d2b8db76f16e
Jen E <jenevans3ten@gmail.com>
2020-09-08 21:34:34 -0400

    more practice code added

--- Program.cs
+++ Program.cs
@@ -12,31 +12,31 @@ namespace csharp_basics_workshop
 
             Console.WriteLine("What is your first name?");
             string firstName = Console.ReadLine();
-
             Console.WriteLine("What is your last name?");
             string lastName = Console.ReadLine();
-
             Console.WriteLine("Hello, " + firstName + " " + lastName);
 
             Console.WriteLine("What do you wish your first name was?");
             firstName = Console.ReadLine();
-
             Console.WriteLine("Then your name would be " + firstName + " " + lastName);
 
             bool isWednesday = true;
             byte dayOfMonth = 9;
             int secondsInMonth = 2592000;
 
-            Console.WriteLine("It's " + isWednesday + " that today is Wednesday.  Today's date is " + dayOfMonth + " and there are " + secondsInMonth + " seconds in this month");
+            Console.WriteLine("It's " + isWednesday + " that today is Wednesday. " +
+                "Today's date is " + dayOfMonth + " and there are " + secondsInMonth + " seconds in this month.");
+            Console.WriteLine("In 5 days the day will be " + (dayOfMonth + 5) + ".");
+            Console.WriteLine("If you divide today's date by 2 the remainder is " + (dayOfMonth % 2) + ".");
+            Console.WriteLine("It is " + (dayOfMonth > 10) + " that today's date is greater than 10.");
 
             double percentMonthComplete;
-            percentMonthComplete = 30 / dayOfMonth;
-
-            Console.WriteLine("This month is " + percentMonthComplete + "% complete");
+            percentMonthComplete = ((double)dayOfMonth / 31) * 100;
+            Console.WriteLine("This month is " + percentMonthComplete + "% complete.");
 
             char firstLetterOfMonth = 'S';
+            Console.WriteLine("This month starts with the letter " + firstLetterOfMonth + ".");
 
-            Console.WriteLine("This month starts with the letter " + firstLetterOfMonth);
         }
     }
 }
