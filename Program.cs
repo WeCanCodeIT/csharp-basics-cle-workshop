5b046a6498487e5bbd84d9f1b17ef1d4bca98b93
Jen E <jenevans3ten@gmail.com>
2020-09-08 20:34:46 -0400

    starter workshop code

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace csharp_basics_workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myFavoriteLanguage = "C#";
+            Console.WriteLine("Let's Learn Some Basics!");
+            Console.WriteLine("Let's Learn" + myFavoriteLanguage);
+
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + firstName + " " + lastName);
+
+            Console.WriteLine("What do you wish your first name was?");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Then your name would be " + firstName + " " + lastName);
+
+            bool isWednesday = true;
+            byte dayOfMonth = 9;
+            int secondsInMonth = 2592000;
+
+            Console.WriteLine("It's " + isWednesday + " that today is Wednesday.  Today's date is " + dayOfMonth + " and there are " + secondsInMonth + " seconds in this month");
+
+            double percentMonthComplete;
+            percentMonthComplete = 30 / dayOfMonth;
+
+            Console.WriteLine("This month is " + percentMonthComplete + "% complete");
+
+            char firstLetterOfMonth = 'S';
+
+            Console.WriteLine("This month starts with the letter " + firstLetterOfMonth);
+        }
+    }
+}
